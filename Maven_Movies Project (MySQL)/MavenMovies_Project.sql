3b6be8c4fc9043638f75757799f0a764f0d8a8cf
Mohamed Elyamany <UG_31155961@science.tanta.edu.eg>
2024-11-05 00:35:34 +0200

    finshed

--- Maven_Movies Project (MySQL)/MavenMovies_Project.sql
+++ Maven_Movies Project (MySQL)/MavenMovies_Project.sql
@@ -1,3 +1,5 @@
+-- Maven_Movies Project --
+
 /* 
 1. My partner and I want to come by each of the stores in person and meet the managers. 
 Please send over the managers’ names at each store, with the full address 
